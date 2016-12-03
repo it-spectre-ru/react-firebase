cd638c5649871e6b8c5ebb4a3dacb17ac1e341bb
tsvetkovpro <285019@gmail.com>
2016-12-03 09:47:21 +0500

    app component

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { RouteHandler } from 'react-router';
+
+
+export default class App extends React.Component {
+	state = { user: USER }
+
+	render() {
+		return <div>
+			<div className='row'>
+				<div className='three columns'>
+					<h1> Wicker </h1>
+					Login
+
+					PageList
+				</div>
+				<div className='nine columns'>
+					<RouteHandler user={this.state.user}/>
+				</div>
+			</div>
+		</div>;
+	}
+}
